32d846378e7ea643ee279b0a4b85b37110ed8693
ANUJ SACHAN <anuj.sachan@akqa.com>
2019-09-04 23:57:45 +1000

    client side redirect

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,12 +23,13 @@ module.exports = {
       options: {
         name: `gatsby-starter-default`,
         short_name: `starter`,
-        start_url: `/page-1`,
+        start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
+      `gatsby-plugin-client-side-redirect`
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,12 @@
  */
 
 // You can delete this file if you're not using it
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createRedirect } = actions //actions is collection of many actions - https://www.gatsbyjs.org/docs/actions
+  createRedirect({
+    fromPath: "/page-2",
+    toPath: "/page-1",
+    isPermanent: true,
+  })
+}
