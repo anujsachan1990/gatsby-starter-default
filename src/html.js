10447707c9ecae6c379c675c148149379739365c
ANUJ SACHAN <anuj.sachan@akqa.com>
2019-10-04 14:12:43 +1000

    qwdqdwq

--- src/html.js
+++ src/html.js
@@ -1,6 +1,8 @@
 import React from "react"
 import PropTypes from "prop-types"
 
+export const isBrowser = () => typeof window !== 'undefined'
+
 export default function HTML(props) {
   return (
     <html {...props.htmlAttributes}>
